ff200369037f3bed28118f71d977cabccecea991
johanOA <johana.ospinao@uqvirtual.edu.co>
2023-06-23 10:43:45 -0500

    Main Banner completed and configured

---
+++ src/components/Banner.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react"
+import { Container, Row, Col } from "react-bootstrap"
+import { ArrowRightCircle } from "react-bootstrap-icons"
+import headerImg from "../assets/img/header-img.svg"
+export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeliting] =useState(false);
+    const toRotate = ["FullStack Developer", "Web/Movile Designer", "UI/UX Designer"];
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta)
+
+        return () => { clearInterval(ticker)};
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+        if(isDeleting){
+            setDelta(prevDelta => prevDelta /2)
+        }
+        if(!isDeleting && updatedText === fullText) {
+            setIsDeliting(true);
+            setDelta(period);
+        } else if(isDeleting && updatedText === '') {
+            setIsDeliting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="aligh-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagLine">Welcome to my Portafolio</span>
+                        <h1>{`Hi I'm Johan `}<span className="wrap">{text}</span></h1>
+                        <p>Computer and systems engineer with experience in Java Full Stack development, web designer and capable in Java, Python, JavaScript, HTML and CSS programming and various frameworks such as Spring, Django, Angular, React, React Native as well as SQL, MySQL and MongoDB Proficient in object oriented programming, microservices and cloud development. Familiar with agile methodologies and the use of modern tools and platforms such as Docker and Github.</p>
+                        <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Header Img" />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,9 @@ import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
-
 export const  NavBar = () => {
     const [activeLink, setActivelink] = useState('home');
     const [scrolled, seScrolled] = useState(false);
-
     useEffect(() => {
         const onScroll = () => {
             if(window.scrollY > 50) {
@@ -17,16 +15,12 @@ export const  NavBar = () => {
                 seScrolled(false)
             }
         }
-
         window.addEventListener("scroll", onScroll);
-
         return () => window.removeEventListener("scroll", onScroll);
     }, [])
-
     const onUpdateActiveLink = (value) => {
         setActivelink(value);
     }
-
     return (
       <Navbar expand="md" className={scrolled ? "scrolled": ""}>
         <Container>
